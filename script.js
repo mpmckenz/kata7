cd9793c48ee15189b34f5c6127464e6c867e9b75
Michael McKenzie <mikemckenzie987@gmail.com>
2018-09-11 14:31:54 -0400

    finished 6

--- script.js
+++ script.js
@@ -1,28 +1,91 @@
 // Kata1 forEach();
 const cohortThreeNames = ["Michael", "James", "Lea", "Carl", "Doug"]
 
-function forEach (cohortThreeNames) {
-    console.log("Hello my name is " + cohortThreeNames + ".")
+function forEach(cohortThreeNames) {
+    document.write("<br>" + "Hello my name is " + cohortThreeNames + "." + "</br>")
 }
 
-function input () {
+function input() {
     for (let i = 0; i < cohortThreeNames.length; i++) {
-    forEach(cohortThreeNames[i])
+        forEach(cohortThreeNames[i])
     }
 }
 input();
 
 // Kata2 map();
-function map(array, callBack) {
-    for (let i = 0; i < hours.length; i++) {
-        array = hours[i]
-        callBack (array, i, hours)
+let array = [1, 2, 3, 4]
+
+function map() {
+    for (let incrementer of array) {
+        incrementer += 1;
+        document.write("<br>" + incrementer + "</br>");
+    }
+}
+map();
+
+// function map(array, callBack) {
+//     for (let i = 0; i < hours.length; i++) {
+//         array = hours[i]
+//         callBack (array, i, hours)
+//     }
+// }
+
+// Kata3 some
+function some(array, callBack) {
+    for (let i = 0; i < array.length; i++) {
+        const arrayFind = array[i];
+        if (arrayFind === array[i]) {
+            return true;
+        } else if (arrayFind !== array[i]) {
+            return false;
+        }
+        callBack(arrayFind, i, array);
     }
 }
 
-// Kata3 some
-let findEvens = [1, 3, 5, 6, 7, 9, 11]
+// function some(findEvens) {
+//     if (findEvens % 2 === 0) {
+//     document.write("true");
+//     } else {
+//         document.write("false");
+//     }
+// }
 
-function some (element) {
-    console.log(element % 2 === 0);
+//Katas4 find()
+function find(array, callBack) {
+    for (let i = 0; i < array.length; i++) {
+        const arraySearch = array[i];
+        if (arraySearch[i] === array[i]) {
+            return arraySearch;
+        } else if (arraySearch !== array[i]) {
+            return "undefined";
+        }
+    }
+    callBack(arraySearch, i, array);
+}
+
+//Katas5 findIndex()
+function findIndex(array, callBack) {
+    for (let i = 0; i < array.length; i++) {
+        const arrayIndex = array[i];
+        if (arrayIndex[i] === array[i]) {
+            return arrayIndex[i]
+        } else if (arrayFind !== array[i]) {
+            return -1;
+        }
+    }
+    callBack(arrayIndex, i, array)
+}
+
+//Katas6 every()
+function every(array, callBack) {
+    for (let i = 0; i < array.length; i++) {
+        const every = array[i];
+        if (every === array) {
+            return true;
+        } else if (every !== array[i]) {
+            return false;
+        }
+    }
+    callBack(every, i, array)
 }
